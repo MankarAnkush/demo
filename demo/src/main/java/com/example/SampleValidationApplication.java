50a05865ae90acbd977ebb37d0b786467bc771b4
Ankush <Ankush@Ankush-PC>
2016-02-07 14:58:06 +0530

    comments added

--- demo/src/main/java/com/example/SampleValidationApplication.java
+++ demo/src/main/java/com/example/SampleValidationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleValidationApplication {
 
+	//main method from here execution starts
 	public static void main(String[] args) {
 		SpringApplication.run(SampleValidationApplication.class, args);
 	}
